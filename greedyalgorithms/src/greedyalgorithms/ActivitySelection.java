601a3c90e604d1301a32e146439cb2ee1bf13a62
satya <satishkumarsama@gmail.com>
2018-08-31 14:13:30 +0530

    commit2-done activity selection program in class

--- greedyalgorithms/src/greedyalgorithms/ActivitySelection.java
+++ greedyalgorithms/src/greedyalgorithms/ActivitySelection.java
@@ -1,5 +1,35 @@
 package greedyalgorithms;
 
 public class ActivitySelection {
+	public static void main(String[] args)
+	{
+		//sort according to finishing time.
+		int s[] = {1,3,0,5,8,5};//starting time 
+		int f[] = {2,4,6,7,9,9};//finishing time
+		int n=s.length;
+		
+		maxActivities(s,f,n);
+		
+	}
+	public static void maxActivities(int s[],int f[], int n)
+	{
+		int i,j;
+		System.out.println("activities that are mutually compatable");
+		//first is selected as it is sorted according to time
+		i=0;
+		System.out.println(i+"");
+		//consider remaining
+		for( j=1;j<n;j++)
+		{
+			//if this activity starting time is greaterthan
+			//or equal to finshing time of previous activity,
+			//than select it.
+			if(s[j]>=f[i])
+			{
+				System.out.println(j+"");
+				i=j;
+			}
+		}
+	}
 
 }
